7c0ce56a27c71b78b88c5d4044e6a983b8be527a
Victor <victormendez.ch7@gmail.com>
2025-01-13 14:59:33 -0600

    Resolución EJercici1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -4,3 +4,6 @@ La felicidad se puede encontrar hasta en los más oscuros
 momentos, si somos capaces de usar bien la luz.
 — Albus Dumbledore '''
 
+print("La felicidad se puede encontrar hasta en los más oscuros ")
+print("momentos, si somos capaces de usar bien la luz.")
+print("— Albus Dumbledore")
